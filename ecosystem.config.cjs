82b43e9ccd9c16ed5097a66e3e3f9a7555913d2b
godwanglin <149907988+godwanglin@users.noreply.github.com>
2026-09-23 08:22:03 +0700

    fix(pm2): set fork mode for next.js in ecosystem config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,6 +5,7 @@ module.exports = {
       script: "node_modules/next/dist/bin/next",
       args: "start -p 3026 -H 0.0.0.0",
       instances: 1,
+      exec_mode: "fork",
       autorestart: true,
       watch: false,
       max_memory_restart: "1G",
